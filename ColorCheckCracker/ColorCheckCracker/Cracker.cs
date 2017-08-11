14e132dea3c9b56745f11f3d6c7f8ef8b36923ed
= <dreamfragment623+weizlogy@gmail.com>
2017-08-12 00:54:30 +0900

    v1.0.0 CCC.

---
+++ ColorCheckCracker/ColorCheckCracker/Cracker.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using static ColorCheckCracker.MainWindow;
+
+namespace ColorCheckCracker {
+    /// <summary>
+    /// color check crack interface.
+    /// </summary>
+    interface Cracker : IDisposable {
+        /// <summary>
+        /// capture the target.
+        /// </summary>
+        /// <param name="pLocation">capture left top axis.</param>
+        /// <param name="pSize">capture width and height.</param>
+        /// <returns>self</returns>
+        Cracker Capture(Point pLocation, Size pSize);
+
+        /// <summary>
+        /// analyze and covert capture image.
+        /// </summary>
+        void Analyze();
+
+        /// <summary>
+        /// get imagesource for image control.
+        /// </summary>
+        /// <returns></returns>
+        BitmapSource GetImageSource();
+    }
+
+    /// <summary>
+    /// <para>color check crack base implementation.</para>
+    /// </summary>
+    abstract class AbstractCracker : Cracker {
+        /// <summary>
+        /// capture image pixcel size.
+        /// </summary>
+        protected const int PIXCEL_SIZE = 4;
+
+        /// <summary>
+        /// capture image.
+        /// </summary>
+        protected System.Drawing.Bitmap CaptureImage { get; private set; }
+
+        /// <summary>
+        /// convert capture image for not max appear colorcode.
+        /// </summary>
+        /// <param name="pixcel">target pixcel</param>
+        /// <param name="index">target index</param>
+        /// <param name="maxApColorCode">max appear colorcode</param>
+        protected abstract void OverlayRemainColors(byte[] pixcel, System.Drawing.Color maxApColorCode);
+
+        /// <summary>
+        /// convert capture image for max appear colorcode.
+        /// </summary>
+        /// <param name="pixcel">target pixcel</param>
+        /// <param name="index">target index</param>
+        /// <param name="maxApColorCode">max appear colorcode</param>
+        protected abstract void OverlayMaxApColor(byte[] pixcel, System.Drawing.Color maxApColorCode);
+
+        /// <summary>
+        /// static factory method.
+        /// </summary>
+        /// <param name="cmode">color check mode</param>
+        /// <returns>cracker interface</returns>
+        public static Cracker Generate(CrackMode cmode) {
+            Cracker cracker = null;
+            switch (cmode) {
+                case CrackMode.Different:
+                    cracker = new DifferentColorCracker();
+                    break;
+                case CrackMode.Same:
+                    cracker = new SameColorCracker();
+                    break;
+            }
+            return cracker;
+        }
+
+        /// <summary>
+        /// <see cref="Cracker.Analyze()"/>
+        /// </summary>
+        public void Analyze() {
+            var bmp = this.CaptureImage;
+            if (bmp == null) {
+                return;
+            }
+            // prepare.
+            var bmpd = bmp.LockBits(
+                new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            var buf = new byte[bmp.Width * bmp.Height * 4];
+            Marshal.Copy(bmpd.Scan0, buf, 0, buf.Length);
+            // find max appear color.
+            var colors = new List<System.Drawing.Color>();
+            for (int i = 0; i < buf.Length; i += PIXCEL_SIZE) {
+                colors.Add(System.Drawing.Color.FromArgb(buf[i + 3], buf[i + 2], buf[i + 1], buf[i]));
+            }
+            var maxApColorInfo =
+                colors.GroupBy(c => c)
+                    .Select(c => new { ColorCode = c.Key, Count = c.Count() })
+                    .OrderByDescending(c => c.Count).First();
+            Console.WriteLine("maxColorAndCount = {0}", maxApColorInfo);
+            // convert.
+            for (int i = 0; i < buf.Length; i += PIXCEL_SIZE) {
+                var pixcel = new byte[PIXCEL_SIZE];
+                Array.Copy(buf, i, pixcel, 0, pixcel.Length);
+                if (colors[i / 4] != maxApColorInfo.ColorCode) {
+                    OverlayRemainColors(pixcel, maxApColorInfo.ColorCode);
+                    Array.Copy(pixcel, 0, buf, i, pixcel.Length);
+                    continue;
+                }
+                OverlayMaxApColor(pixcel, maxApColorInfo.ColorCode);
+                Array.Copy(pixcel, 0, buf, i, pixcel.Length);
+            }
+            // end.
+            Marshal.Copy(buf, 0, bmpd.Scan0, buf.Length);
+            bmp.UnlockBits(bmpd);
+            //bmp.Save(@"E:\test.png", ImageFormat.Png);
+        }
+
+        /// <summary>
+        /// <see cref="Cracker.Capture()"/>
+        /// </summary>
+        public Cracker Capture(Point pLocation, Size pSize) {
+            var bmp = new System.Drawing.Bitmap((int)pSize.Width, (int)pSize.Height, PixelFormat.Format32bppArgb);
+            using (var grph = System.Drawing.Graphics.FromImage(bmp)) {
+                grph.CopyFromScreen((int)pLocation.X, (int)pLocation.Y, 0, 0, bmp.Size);
+            }
+            this.CaptureImage = bmp;
+            return this;
+        }
+
+        /// <summary>
+        /// dispose capture image.
+        /// </summary>
+        public void Dispose() {
+            if (this.CaptureImage == null) {
+                return;
+            }
+            this.CaptureImage.Dispose();
+        }
+
+        /// <summary>
+        /// <see cref="Cracker.GetImageSource()"/>
+        /// </summary>
+        public BitmapSource GetImageSource() {
+            var bmp = this.CaptureImage;
+            return Imaging.CreateBitmapSourceFromHBitmap(
+                bmp.GetHbitmap(),
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+        }
+    }
+
+    /// <summary>
+    /// <para>different color check crack implementation.</para>
+    /// <para>
+    /// convert to reverse color for max appear color. 
+    /// convert to clear color for otherwise.
+    /// </para>
+    /// </summary>
+    class DifferentColorCracker : AbstractCracker {
+        protected override void OverlayRemainColors(byte[] pixcel, System.Drawing.Color maxApColorCode) {
+            // clear alpha channel.
+            pixcel[3] = 0x00;
+        }
+        protected override void OverlayMaxApColor(byte[] pixcel, System.Drawing.Color maxApColorCode) {
+            // reverse color.
+            var index = 0;
+            pixcel[index++] = (byte)(0xFF - maxApColorCode.B);
+            pixcel[index++] = (byte)(0xFF - maxApColorCode.G);
+            pixcel[index++] = (byte)(0xFF - maxApColorCode.R);
+        }
+    }
+
+    /// <summary>
+    /// <para>same color check crack implementation.</para>
+    /// <para>
+    /// convert to clear color for max appear color. 
+    /// convert to reverse color for otherwise.
+    /// </para>
+    /// </summary>
+    class SameColorCracker : AbstractCracker {
+        protected override void OverlayRemainColors(byte[] pixcel, System.Drawing.Color maxApColorCode) {
+            // reverse color.
+            var index = 0;
+            pixcel[index++] = (byte)(0xFF - maxApColorCode.B);
+            pixcel[index++] = (byte)(0xFF - maxApColorCode.G);
+            pixcel[index++] = (byte)(0xFF - maxApColorCode.R);
+        }
+        protected override void OverlayMaxApColor(byte[] pixcel, System.Drawing.Color maxApColorCode) {
+            // clear alpha channel.
+            pixcel[3] = 0x00;
+        }
+    }
+}
---
+++ ColorCheckCracker/ColorCheckCracker/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorCheckCracker {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        /// <summary>
+        /// <para>color check crack mode.</para>
+        /// </summary>
+        public enum CrackMode {
+            /// <summary>
+            /// choose one that it has same target color.
+            /// </summary>
+            Same,
+            /// <summary>
+            /// choose one that it has different target color.
+            /// </summary>
+            Different
+        }
+
+        /// <summary>
+        /// true, if it's capturing display. false, otherwise.
+        /// </summary>
+        private bool capturing = false;
+        /// <summary>
+        /// current color check crack mode.
+        /// </summary>
+        private CrackMode cmode = CrackMode.Different;
+
+        /// <summary>
+        /// initialize UI and register event handers.
+        /// </summary>
+        public MainWindow() {
+            // auto generate code.
+            InitializeComponent();
+            // mouse left to drag window.
+            this.MouseLeftButtonDown += (sender, e) => { this.DragMove(); };
+            // deactivate to activate.
+            // ignore activate when capturing or closing.
+            this.Deactivated += (sender, e) => {
+                if (this.capturing || this.Tag != null) {
+                    return;
+                }
+                // if no delay, often do not activated.
+                Task.Delay(200).ContinueWith(_ => {
+                    this.Dispatcher.Invoke(() => {
+                        this.Activate();
+                    });
+                });
+            };
+            // closing to ignore deactivate event.
+            this.Closing += (sender, e) => {
+                this.Tag = new object();
+            };
+            // key control.
+            // escape to clear image or close window.
+            // enter to start crack.
+            // S to Same mode.
+            // D to Different mode.
+            // T to Terminate temporary transparent window.
+            this.KeyUp += (sender, e) => {
+                switch (e.Key) {
+                    case Key.Escape:
+                        if (this.pic.Source != null) {
+                            ClearImage();
+                            break;
+                        }
+                        Application.Current.Shutdown();
+                        break;
+                    case Key.Enter:
+                        DoCrack();
+                        break;
+                    case Key.S:
+                        cmode = CrackMode.Same;
+                        this.BorderBrush = Brushes.Red;
+                        ClearImage();
+                        break;
+                    case Key.D:
+                        cmode = CrackMode.Different;
+                        this.BorderBrush = Brushes.Black;
+                        ClearImage();
+                        break;
+                    case Key.T:
+                        this.Background.Opacity = this.Background.Opacity == 0 ? 1 : 0;
+                        break;
+                    default:
+                        break;
+                }
+            };
+            // key control.
+            // enter to clear image.
+            // T to Temporary transparent window.
+            this.KeyDown += (sender, e) => {
+                switch (e.Key) {
+                    case Key.Enter:
+                        ClearImage();
+                        break;
+                    default:
+                        break;
+                }
+            };
+        }
+
+        /// <summary>
+        /// clear capture image.
+        /// </summary>
+        public void ClearImage() {
+            this.pic.Source = null;
+            this.Background.Opacity = 1;
+        }
+
+        /// <summary>
+        /// <para>execute crack.</para>
+        /// </summary>
+        public void DoCrack() {
+            this.capturing = true;
+            // keep location and size.
+            var screenLocation = this.PointToScreen(new Point());
+            var windowSize = this.RenderSize;
+            Console.WriteLine("capture axis x={0}, y={1} size w={2}, h={3}",
+                screenLocation.X, screenLocation.Y, windowSize.Width, windowSize.Height);
+            // crack and set result to window.
+            using (Cracker cracker = AbstractCracker.Generate(this.cmode)) {
+                cracker.Capture(screenLocation, windowSize).Analyze();
+                this.pic.Source = cracker.GetImageSource();
+            }
+            Console.WriteLine("Capture end.");
+            this.capturing = false;
+            // transparent windos for click under the window.
+            this.Background.Opacity = 0;
+        }
+    }
+}
